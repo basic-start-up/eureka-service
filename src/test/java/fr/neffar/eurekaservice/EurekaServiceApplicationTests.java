d850f8afe3aced2b8e98c5619c5142ac8132ec9c
neffar <soufiane.neffar@gmail.com>
2021-12-26 21:36:05 +0100

    Initial commit

---
+++ src/test/java/fr/neffar/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.neffar.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
